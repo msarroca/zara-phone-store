0a99f18fb08569bdadfba55ee4b391bc11c90e76
Marc <m.sarroca@gmail.com>
2025-10-16 00:14:25 +0200

    feat: add new deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,13 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next",
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:prettier/recommended",
+    "plugin:jsx-a11y/recommended"
+  ),
   {
     ignores: [
       "node_modules/**",
@@ -19,6 +25,38 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      "prettier/prettier": [
+        "error",
+        {
+          arrowParens: "avoid",
+          bracketSameLime: false,
+          bracketSpacing: false,
+          endOfLine: "auto",
+          optionalChaining: true,
+          printWidth: 120,
+          semi: false,
+          singleQuote: true,
+          tabWidth: 2,
+          trailingComma: "none",
+          useTabs: false,
+        },
+        {
+          usePrettierrc: false,
+        },
+      ],
+      "prefer-const": "error",
+      "no-undef": "error",
+      "no-console": "warn",
+      "no-only-tests/no-only-tests": "error",
+      "react/react-in-jsx-scope": "off",
+      "jsx-a11y/alt-text": "warn",
+      "jsx-a11y/aria-props": "warn",
+      "jsx-a11y/aria-proptypes": "warn",
+      "jsx-a11y/aria-unsupported-elements": "warn",
+      "jsx-a11y/role-has-required-aria-props": "warn",
+      "jsx-a11y/role-supports-aria-props": "warn",
+    },
   },
 ];
 

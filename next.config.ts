95bb7de6352f929579faee31c83245c1fd3ade38
Marc <m.sarroca@gmail.com>
2025-10-16 10:55:16 +0200

    feat: add next and jsconfig

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "prueba-tecnica-api-tienda-moviles.onrender.com",
+        port: "",
+        pathname: "/images/**",
+        search: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;

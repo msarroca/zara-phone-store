bd3e78011d05c3ccceb82cd491ce0162993aa5af
Marc <m.sarroca@gmail.com>
2025-10-18 14:46:42 +0200

    feat: add error and loading components

---
+++ src/app/error.js
@@ -0,0 +1,10 @@
+'use client';
+
+const Error = () => (
+  <div style={{ textAlign: 'center', padding: '2rem' }}>
+    <h2>Oops! Something went wrong!</h2>
+  </div>
+);
+
+Error.displayName = 'Error';
+export default Error;
---
+++ src/app/loading.js
@@ -0,0 +1,8 @@
+const Loading = () => (
+  <div style={{ textAlign: 'center', padding: '2rem' }}>
+    <p>Loading product...</p>
+  </div>
+);
+
+Loading.displayName = 'Loading';
+export default Loading;
